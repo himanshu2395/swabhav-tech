c96e4524d15eb69bc03971764eaee515db501592
himanshu2395 <himanshucgc23@gmail.com>
2018-01-18 08:32:08 +0530

    modified TestAdd

--- Core java/OOP/Overloading-App/src/com/techlabs/overloading/TestAdd.java
+++ Core java/OOP/Overloading-App/src/com/techlabs/overloading/TestAdd.java
@@ -5,11 +5,72 @@ public class TestAdd {
 	public static void main(String[] args) {
 		// add();
 		// add(30, 40);
-		roundof((float) 5.6);
-		roundof((float) 7.3);
-		roundof((float) 8.4);
-		System.out.println("is 10 even?" + iseven(10));
+		// roundof((float) 5.6);
+		// roundof((float) 7.3);
+		// roundof((float) 8.4);
+		// System.out.println("is 10 even?" + iseven(10));
+		// System.out.println("is 115 even?" + iseven(115));
+		 printEven(100, 210);
+		//printEven(100);
+		 System.out.println(isPalindrome(202));
 
+	}
+	
+
+
+	private static boolean isPalindrome(int number) {
+		 int reverse = 0, remainder, originalInteger;
+
+	        originalInteger = number;
+
+	       
+	        while( number != 0 )
+	        {
+	            remainder = number % 10;
+	            reverse = reverse * 10 + remainder;
+	            number  /= 10;
+	        }
+
+	        if (originalInteger == reverse) {
+	        return true;
+	        }else {
+	        return false;
+	    }
+	}
+	
+		
+	
+
+
+
+	static void printEven(int start, int end) {
+		int count = 0;
+
+		while (start <= end) {
+			start++;
+			if (start % 2 == 0) {
+				count++;
+				System.out.println(start + " ");
+				if(isPalindrome(start)) {
+					System.out.println("number is palindrome"+start);
+				}
+
+			}
+		}
+		System.out.println("Numbers are present in between=" + count);
+		System.out.println();
+	}
+
+	static void printEven(int start) {
+		int count = 0;
+		while (start <= 300) {
+			start++;
+			if (start % 2 == 0) {
+				count++;
+				System.out.println(start + " ");
+			}
+		}
+		System.out.println("Numbers are present in between=" + count);
 	}
 
 	static boolean iseven(int i) {
