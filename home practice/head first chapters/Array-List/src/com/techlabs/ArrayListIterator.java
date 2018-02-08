dd6938462dc957ebb80b7e37ef609109369088f8
himanshu2395 <himanshucgc23@gmail.com>
2018-02-08 20:53:19 +0530

    add

--- home practice/head first chapters/Array-List/src/com/techlabs/ArrayListIterator.java
+++ home practice/head first chapters/Array-List/src/com/techlabs/ArrayListIterator.java
@@ -7,17 +7,41 @@ public class ArrayListIterator {
 	public static void main(String[] args) {
 
 		ArrayList<String> name = new ArrayList<String>();
+		// ADDING
 		name.add("Himanshu");
 		name.add("Rahul");
 		name.add("Manish");
 		name.add("jyoti");
 		name.add("Swati");
 		name.add("neha");
-		Iterator<String> itr = name.iterator();
-		while (itr.hasNext()) {
-			System.out.println(itr.next());
+		// RETRIVING
+		System.out.println(name.get(1));
+
+		// RETRIVING USING ITERATOR
+
+		// Iterator<String> itr = name.iterator();
+		// while (itr.hasNext()) {
+		// System.out.println(itr.next());
+
+		// INDEX FOR LOOP ITERATION
+
+		for (int i = 0; i < name.size(); i++) {
+			System.out.println(name.get(i));
 		}
+		System.out.println();
 
+		// REMOVING NAME FROM A LIST
+
+		name.remove(name.size() - 1);
+
+		// This is Slow
+
+		name.remove(0);
+
+		// ITERATION N0.2
+
+		for (String xyz : name) {
+			System.out.println(xyz);
+		}
 	}
-
 }
--- home practice/head first chapters/Array-List/src/com/techlabs/ReverseArrayList.java
+++ home practice/head first chapters/Array-List/src/com/techlabs/ReverseArrayList.java
@@ -12,14 +12,10 @@ public class ReverseArrayList {
 		name.add("mark");
 		name.add("Patrik");
 		name.add("jack");
-		name.remove(3);
-		// name.clear();
 		name.add(0, "Swati");
-		for (String x : name) {
-			System.out.println(x);
-		}
-		System.out.println();
+		System.out.println(name);
 		Collections.reverse(name);
 		System.out.println(name);
 	}
+
 }
